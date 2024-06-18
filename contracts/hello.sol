f5fa4478e114ab273f7c22c8673b0474d754a56e
Victor-Muriithi <vmuriithi10@gmail.com>
2024-06-18 18:56:16 +0300

    Initial commit

---
+++ contracts/hello.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+contract Hello {
+    string public welcome = "Hello World!!";
+    bool public state = true;
+    uint8 public MAX_SUPPLY = 10;
+    uint256 public WAIT_TIME = 1 days;
+} 
